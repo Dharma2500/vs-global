9284b766092844e535e555e76a28107fb71f036d
Dharma2500 <52962716+Dharma2500@users.noreply.github.com>
2026-03-09 08:49:39 +0500

    Update Tab2.java

--- src/main/java/com/vs/vscombo/client/screen/tabs/Tab2.java
+++ src/main/java/com/vs/vscombo/client/screen/tabs/Tab2.java
@@ -3,12 +3,12 @@ package com.vs.vscombo.client.screen.tabs;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 
-public class Tab2 implements ITab { // ✅ Класс называется Tab2, файл Tab2.java
+public class Tab2 implements ITab {
     @Override
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
         Minecraft mc = Minecraft.getInstance();
-        mc.font.drawString(matrixStack, "Content of Tab 2", 10f, 10f, 0xFFFFFF);
-        mc.font.drawString(matrixStack, "This is test2 tab content", 10f, 25f, 0xAAAAAA);
+        mc.font.draw(matrixStack, "Content of Tab 2", 10f, 10f, 0xFFFFFF);
+        mc.font.draw(matrixStack, "This is test2 tab content", 10f, 25f, 0xAAAAAA);
     }
     @Override public String getTabId() { return "tab2"; }
     @Override public String getTabName() { return "test2"; }
