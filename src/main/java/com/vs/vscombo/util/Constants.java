0a5fdc35ee408cb8889b4162ea56adc6db3299d6
Dharma2500 <52962716+Dharma2500@users.noreply.github.com>
2026-03-09 08:13:53 +0500

    Create Constants.java

---
+++ src/main/java/com/vs/vscombo/util/Constants.java
@@ -0,0 +1,24 @@
+package com.vs.vscombo.util;
+
+public class Constants {
+    // GUI Constants
+    public static final int GUI_WIDTH = 400;
+    public static final int GUI_HEIGHT = 250;
+    public static final int BUTTON_WIDTH = 100;
+    public static final int BUTTON_HEIGHT = 20;
+    public static final int BUTTON_X = 5;
+    public static final int BUTTON_Y_START = 25;
+    public static final int BUTTON_SPACING = 5;
+    
+    // Content area
+    public static final int CONTENT_X = 115;
+    public static final int CONTENT_Y = 10;
+    public static final int CONTENT_WIDTH = 275;
+    public static final int CONTENT_HEIGHT = 230;
+    
+    // Tab names (internal)
+    public static final String[] TAB_IDS = {"tab1", "tab2", "tab3", "tab4", "tab5"};
+    
+    // Button labels (display)
+    public static final String[] TAB_BUTTONS = {"test1", "test2", "test3", "test4", "test5"};
+}
