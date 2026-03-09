a3229263122ead186632435a172e2b3bfd4fdf33
Dharma2500 <52962716+Dharma2500@users.noreply.github.com>
2026-03-09 09:46:39 +0500

    Update Constants.java

--- src/main/java/com/vs/vscombo/util/Constants.java
+++ src/main/java/com/vs/vscombo/util/Constants.java
@@ -10,15 +10,17 @@ public class Constants {
     public static final int BUTTON_Y_START = 25;
     public static final int BUTTON_SPACING = 5;
     
-    // Content area
     public static final int CONTENT_X = 115;
     public static final int CONTENT_Y = 10;
     public static final int CONTENT_WIDTH = 275;
     public static final int CONTENT_HEIGHT = 230;
     
-    // Tab names (internal)
-    public static final String[] TAB_IDS = {"tab1", "tab2", "tab3", "tab4", "tab5"};
+    // ✅ Обновлённые keys для локализации
+    public static final String GUI_TITLE_KEY = "gui.vsglobal.mythical_equipment";
+    public static final String KEY_OPEN_GUI = "key.vsglobal.open_gui";
+    public static final String KEY_CATEGORY = "key.categories.vsglobal";
     
-    // Button labels (display)
+    // Tab names (internal - не меняем, это логика, не локализация)
+    public static final String[] TAB_IDS = {"tab1", "tab2", "tab3", "tab4", "tab5"};
     public static final String[] TAB_BUTTONS = {"test1", "test2", "test3", "test4", "test5"};
 }
