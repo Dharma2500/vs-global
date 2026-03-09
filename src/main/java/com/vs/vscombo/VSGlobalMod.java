e2b4e0fbdb50f99de0f6af6dd0c32110b9976a6b
Dharma2500 <52962716+Dharma2500@users.noreply.github.com>
2026-03-10 00:56:30 +0500

    Update VSGlobalMod.java

--- src/main/java/com/vs/vscombo/VSGlobalMod.java
+++ src/main/java/com/vs/vscombo/VSGlobalMod.java
@@ -1,6 +1,12 @@
 package com.vs.vscombo;
 
+import com.vs.vscombo.client.keybind.ModKeyBindings;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,9 +14,17 @@ import org.apache.logging.log4j.Logger;
 public class VSGlobalMod {
     
     public static final Logger LOGGER = LogManager.getLogger();
-    public static final String MODID = "vsglobal";  // ✅ Правильное имя: MODID (без подчёркивания)
+    public static final String MODID = "vsglobal";
     
     public VSGlobalMod() {
         LOGGER.info("VS Global Mod initialized with ID: {}", MODID);
+        
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        
+        // Регистрируем ключи на клиенте
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> () -> {
+            ModKeyBindings.register();
+            MinecraftForge.EVENT_BUS.register(this);
+        });
     }
 }
