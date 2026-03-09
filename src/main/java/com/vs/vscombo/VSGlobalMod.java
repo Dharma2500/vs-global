7431b5db95ff3d78180f94315be77be56e4f5554
Dharma2500 <52962716+Dharma2500@users.noreply.github.com>
2026-03-09 09:43:42 +0500

    Update VSGlobalMod.java

--- src/main/java/com/vs/vscombo/VSGlobalMod.java
+++ src/main/java/com/vs/vscombo/VSGlobalMod.java
@@ -6,19 +6,16 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(VSGlobalMod.MOD_ID)
+@Mod(VSGlobalMod.MOD_ID) // ✅ Использует константу ниже
 public class VSGlobalMod {
-    public static final String MOD_ID = "vscombo";
+    public static final String MOD_ID = "vsglobal"; // ✅ Уникальный ID без дефиса
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     
     public static VSGlobalMod INSTANCE;
     
     public VSGlobalMod() {
         INSTANCE = this;
-        
-        // Регистрируем мод в Forge EventBus (для общих событий)
         MinecraftForge.EVENT_BUS.register(this);
-        
-        LOGGER.info("VS Global Mod initialized!");
+        LOGGER.info("VS Global Mod initialized with ID: {}", MOD_ID);
     }
 }
